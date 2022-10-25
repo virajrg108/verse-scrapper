8ab09cadcc797a119687fa2b4a155c6f4988138c
virajrg108 <viraj.gawde123@gmail.com>
2022-10-25 13:56:56 +0530

    scrapped verse 1.1.1

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-html = requests.get('https://vedabase.io/en/library/sb/1/1/1')
-parsed_html = BeautifulSoup(html.text)
-print(parsed_html.body.find('div', attrs={'id': 'bb13392'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb554882'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb13393'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb13394'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb13394'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb13395'}).decode_contents())
-print(parsed_html.body.find('div', attrs={'id':'bb13395'}).decode_contents())
+def scrap_sb():
+    html = requests.get('https://vedabase.io/en/library/sb/1/1/1')
+    parsed_html = BeautifulSoup(html.text)
+    verseNbr = parsed_html.body.find('div', attrs={'id': 'bb13392'}).decode_contents()
+    sanskritText = parsed_html.body.find('div', attrs={'id':'bb554882'}).decode_contents()
+    englishText = parsed_html.body.find('div', attrs={'id':'bb13393'}).decode_contents()
+    synonyms = parsed_html.body.find('div', attrs={'id':'bb13394'}).decode_contents()
+    translation = parsed_html.body.find('div', attrs={'id':'bb13395'}).decode_contents()
+    purport = parsed_html.body.find('div', attrs={'class':'wrapper-puport'})
+
+    for title in purport.select('h2.section-title'):
+        title.decompose()
+
+    print(purport)
+
+if __name__ == "__main__":
+    scrap_sb()
+
